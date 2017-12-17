5d8fad49cd0d731cd285901f2ef9af23404b8c9c
Satish Gandham <satish.iitg@gmail.com>
2017-12-18 00:35:44 +0530

    * Add pink color scheme.

--- inc/admin/color-schemes.php
+++ inc/admin/color-schemes.php
@@ -15,6 +15,18 @@ $header-bg:$primary-nav-bg;
 }
 ';
 
+$page_speed_color_schemes['pink'] = '
+$primary-nav-bg:#c43e74;
+$primary-nav-color:#FFF;
+$header-bg:$light-1;
+$sb-widget-title-bg:lighten(#c43e74,.2);
+$sb-widget-ttile-color: $dark-6;
+@if($is_sleek_header == 1){
+$header-bg:$primary-nav-bg;
+}
+
+';
+
 
 function pagespeed_get_color_scheme_choices() {
 	GLOBAL $page_speed_color_schemes;
