0d2d7bf19c740095428917d905f7ede264071e6f
Satish Gandham <satish.iitg@gmail.com>
2018-01-01 15:37:33 +0530

    Add line break in setup navigation prompt.

--- inc/functions-display.php
+++ inc/functions-display.php
@@ -206,7 +206,7 @@ function pagespeed_breadcrumbs() {
 function page_speed_dummy_menu() {
 
 	if ( current_user_can( 'customize' ) && ! has_nav_menu( 'primary' ) && ! has_nav_menu( 'header' ) ) {
-		echo '<div style="text-align: center"> <a href="#" >Howdy!! Thanks for choosing PageSpeed :-). Set the primary navigation menu at <strong>appearance -> menus</strong> and I will go away !!</a></div>';
+		echo '<div style="text-align: center"> <a href="#" >Howdy!! Thanks for choosing PageSpeed :-).<br />Set the primary navigation menu at <strong>appearance -> menus</strong> and I will go away !!</a></div>';
 	}
 
 	return null;
