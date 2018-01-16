87b3ed440d49ef2c1edfb6877d270292f0dc3e89
Satish Gandham <satish.iitg@gmail.com>
2018-01-16 21:23:52 +0530

    * Remove settings page.

--- inc/admin/customize/customizer.php
+++ inc/admin/customize/customizer.php
@@ -228,43 +228,7 @@ function pagespeed_customize( $wp_customize ) {
 	);
 
 
-//	$wp_customize->add_setting( 'dedicated_sidebars_on_home', array(
-//		'sanitize_callback' => 'helium_boolean',
-//		'default'           => false,
-//	) );
-//
-//	$wp_customize->add_control( 'dedicated_sidebars_on_home', array(
-//		'label'       => __( 'Dedicated sidebars for home page', 'page-speed' ),
-//		'description' => __( 'Create separate sidebars for home page.', 'page-speed' ),
-//		'section'     => 'layout_settings',
-//		'type'        => 'checkbox',
-//		'priority'    => 30,
-//	) );
-//	$wp_customize->add_setting( 'dedicated_sidebars_on_default_page_template', array(
-//		'sanitize_callback' => 'helium_boolean',
-//		'default'           => false,
-//	) );
-//
-//	$wp_customize->add_control( 'dedicated_sidebars_on_default_page_template', array(
-//		'label'       => __( 'Dedicated sidebars for default page template', 'page-speed' ),
-//		'description' => __( ' Create separate sidebars for home page.', 'page-speed' ),
-//		'section'     => 'layout_settings',
-//		'type'        => 'checkbox',
-//		'priority'    => 30,
-//	) );
-//
-//	$wp_customize->add_setting( 'enable_sticky_sidebars', array(
-//		'sanitize_callback' => 'helium_boolean',
-//		'default'           => false,
-//	) );
-//
-//	$wp_customize->add_control( 'enable_sticky_sidebars', array(
-//		'label'       => __( 'Create sticky sidebars', 'page-speed' ),
-//		'description' => __( 'Sticky sidebars or floating sidebars are sidebars that stick/float on the screen when the user is scrolling the page.', 'page-speed' ),
-//		'section'     => 'layout_settings',
-//		'type'        => 'checkbox',
-//		'priority'    => 30,
-//	) );
+
 
 }
 
