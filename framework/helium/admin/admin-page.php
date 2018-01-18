5f2441225df62a32aaf88ba3d50d41acaad48a95
Satish Gandham <satish.iitg@gmail.com>
2018-01-18 10:54:20 +0530

    * Remove pagespeed from stransaltions.

--- framework/helium/admin/admin-page.php
+++ framework/helium/admin/admin-page.php
@@ -14,8 +14,8 @@ function helium_theme_menu() {
 
 
 	$helium_theme_options = add_theme_page(
-		__('PageSpeed Theme Helpers','page-speed'),            // The title to be displayed in the browser window for this page.
-		__('PageSpeed Helpers'),            // The text to be displayed for this menu item
+		'PageSpeed ' . __( 'Theme Helpers', 'page-speed' ),            // The title to be displayed in the browser window for this page.
+		'PageSpeed ' . __( 'Helpers' ),            // The text to be displayed for this menu item
 		'administrator',            // Which type of users can see this menu item
 		'helium_theme_options',    // The unique ID - that is, the slug - for this menu item
 		'helium_theme_options_display'     // The name of the function to call when rendering this menu's page
@@ -31,11 +31,11 @@ function pagespeed_help() {
 	?>
     <div class="wrap">
         <p style="font-size:20px;font-weight: 300">
-            <?php _e('Helium stores the SASS/CSS content to the database in a transient for a smoother experience on the theme customizer.<br/>
-            If you switched or upgraded your theme, and the customizer is not working as expected, try clearing the transients.','page-speed')?>
+			<?php _e( 'Helium stores the SASS/CSS content to the database in a transient for a smoother experience on the theme customizer.<br/>
+            If you switched or upgraded your theme, and the customizer is not working as expected, try clearing the transients.', 'page-speed' ) ?>
         </p>
         <button id="clear-sass" class="button button-primary"
-                style="padding:10px 20px;font-size:24px;border-radius: 40px;height: auto"><?php _e('Clear Transients','page-speed')?>
+                style="padding:10px 20px;font-size:24px;border-radius: 40px;height: auto"><?php _e( 'Clear Transients', 'page-speed' ) ?>
         </button>
         <br/>
         <span id="clear_cache_results"></span>
@@ -43,23 +43,23 @@ function pagespeed_help() {
 
         <hr/>
         <p style="font-size:20px;font-weight: 300">
-	<?php _e('Helium saves your file system status on whether it can write the stylesheet to uploads directory or not.
+			<?php _e( 'Helium saves your file system status on whether it can write the stylesheet to uploads directory or not.
             It loads a pre-compiled stylesheet if it can not write to uploads and your layout and color changes might
             not work.
-            You can force a recheck here.','page-speed')?>
+            You can force a recheck here.', 'page-speed' ) ?>
         </p>
         <p>
 			<?php
 			if ( get_theme_mod( 'can_read_write' ) ) {
-				echo '            <span id="can-write" class="options-status" style="display: inline;">'.__('Can read & write','page-speed').'</span>';
+				echo '            <span id="can-write" class="options-status" style="display: inline;">' . __( 'Can read & write', 'page-speed' ) . '</span>';
 			} else {
-				echo '            <span id="can-not-write" class="options-status" style="display: inline;">'.__('Can not read & write','page-speed').'</span>';
+				echo '            <span id="can-not-write" class="options-status" style="display: inline;">' . __( 'Can not read & write', 'page-speed' ) . '</span>';
 			}
 			?>
         </p>
         <button id="update-write-status" class="button button-primary"
                 style="padding:10px 20px;font-size:24px;border-radius: 40px;height: auto">
-            <?php _e('Update File System Status','page-speed')?>
+			<?php _e( 'Update File System Status', 'page-speed' ) ?>
         </button>
         <br/>
         <span id="clear_write_status_results"></span>
