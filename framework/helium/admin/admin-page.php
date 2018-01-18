17c5d5084d78ca80bd43d0e7699fccc1de3685e1
Satish Gandham <satish.iitg@gmail.com>
2018-01-18 11:00:02 +0530

    * Fix an issue with translation.

--- framework/helium/admin/admin-page.php
+++ framework/helium/admin/admin-page.php
@@ -15,7 +15,7 @@ function helium_theme_menu() {
 
 	$helium_theme_options = add_theme_page(
 		'PageSpeed ' . __( 'Theme Helpers', 'page-speed' ),            // The title to be displayed in the browser window for this page.
-		'PageSpeed ' . __( 'Helpers' ),            // The text to be displayed for this menu item
+		'PageSpeed ' . __( 'Helpers','page-speed' ),            // The text to be displayed for this menu item
 		'administrator',            // Which type of users can see this menu item
 		'helium_theme_options',    // The unique ID - that is, the slug - for this menu item
 		'helium_theme_options_display'     // The name of the function to call when rendering this menu's page
